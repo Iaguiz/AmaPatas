3551e9a911e5fd962f70f56495e2ab09009aeb5a
Iago Reis Homsi <iagohomsi@gmail.com>
2024-04-16 18:47:37 -0300

    Adicionando icones e correções

--- site-amapatas/script.js
+++ site-amapatas/script.js
@@ -1,9 +1,7 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-    nextImage()
-}, 10000)
+setInterval( function(){nextImage()}, 10000)
 
 function nextImage(){
     count++;
@@ -12,4 +10,7 @@ function nextImage(){
     }
     
     document.getElementById("radio"+count).checked = true;
+    /*if(document.getElementById("radio"+count).checked = false){
+        
+    }*/
 }
