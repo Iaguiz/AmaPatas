f05b7a87b6b4572b7f55c1c6509994a4f96765ca
Iago Reis Homsi <iagohomsi@gmail.com>
2024-05-03 15:38:48 -0300

    Movimentação e remoção de algumas imagens e arquivos
    Removido imagens de animais que estava sendo utilizado de "placeholder"
    
    Tentei utilizar o REACT porém bugou bastante, então desfiz para refazer em outro momento

--- site-amapatas/src/App.jsx
+++ site-amapatas/src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
+import Header from './header.jsx'
+import Slider from './slider.jsx'
 
+function App() {
+    return(
+        <>
+            <Header></Header>
+            <Slider></Slider>
+        </>
+    );
 }
 
 export default App
